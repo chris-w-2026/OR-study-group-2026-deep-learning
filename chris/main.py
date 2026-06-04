2a518ec092f98e5b114eef6a6559e9a65b3d3a91
chris-w-2026 <christopher.wiseman@homeoffice.gov.uk>
2026-06-04 16:40:15 +0000

    Initial code to test github commits

--- chris/main.py
+++ chris/main.py
@@ -1,1 +1,12 @@
 import numpy as np
+
+
+class NeuralNetwork:
+    def __init__(self, layers: list[int]):
+        self.layers = layers
+
+
+
+if __name__ == "__main__":
+    net = NeuralNetwork([3, 5, 5, 2])
+    print(net.layers)
